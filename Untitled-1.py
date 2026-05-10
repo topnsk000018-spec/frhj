f95e9767b2051ef43c26397f0e840b9b5bd3bc6a
topnsk000018-spec <topnsk000018@gmail.com>
2026-05-10 14:18:25 +0700

    python 2

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,13 +1,17 @@
-print("hello world")
-age = input("Введите ваш возраст")
-print("----------------")
-print(age + "ваш возраст" + "\n hhh")
-print("----------------")
+import math
 
-print("😍😺🤑")
-print("привет")
-print("----------------")
+a = float(input("10"))
+b = float(input("8"))
+hypotenuse = math.sqrt(a**2 + b**2)
+area= 0.5*a*b
+print(f"Гипотенуза: {hypotenuse}")
+print(f"Площадь: {area}")
 
-print("мой возраст 14")
-print("----------------")
-print("(●'◡'●)")
+
+
+
+a = int(input("25"))
+b = int(input("5"))
+целая_часть = a // b 
+остаток = a % b
+print(f"Целая часть: {целая_часть} остаток: {остаток})
